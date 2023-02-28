9fb5368d9eb4df705b5422193772cc9b616770f7
Jinu George <jinugeo@gmail.com>
2023-02-28 14:53:51 -0800

    Changes to the CaPayData.js file

--- CAPayData.js
+++ CAPayData.js
@@ -1,7 +1,10 @@
 function displaymyMessage()
 {
-	t5U13File = document.getElementById("T5U13").files[0]; 
-document.getElementById('ui_message').innerHTML =t5U13File.name ;  
+   t5U13File = document.getElementById("T5U13").files[0]; 
+
+   cares = document.getElementById("CARESDTA").files[0];
+   document.getElementById('ui_message').innerHTML =t5U13File.name + "----" + cares.name;  
+  
 
 
 
