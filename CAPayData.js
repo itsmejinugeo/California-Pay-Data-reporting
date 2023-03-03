a976f7e304c5385dcc8b8fbe843605fe65b2118f
Jinu George <jinugeo@gmail.com>
2023-03-03 12:51:17 -0800

    Commit after the homeTest file

--- CAPayData.js
+++ CAPayData.js
@@ -2,9 +2,11 @@ function displaymyMessage()
 {
    t5U13File = document.getElementById("T5U13").files[0]; 
 
-   cares = document.getElementById("CARESDTA").files[0];
-   document.getElementById('ui_message').innerHTML =t5U13File.name + "----" + cares.name;  
+   //cares = document.getElementById("CARESDTA").files[0];
+   //document.getElementById('ui_message').innerHTML =t5U13File.name + "----" + cares.name;  
   
+   window.location.href="C:/Users/jgeorge/Desktop/ECC/California%20PayData/homeTest.html";
+   //window.location.replace("https://www.costco.com/");
 
 
 
